0e3edaea6ea8e06d9776a0fb6257d821cf061386
jungwoohyun <jung2929@naver.com>
2016-10-08 04:49:31 +0900

    로그인완료, 회원가입하는중

---
+++ app/common/sqlConverter.scala
@@ -0,0 +1,74 @@
+package common
+
+import play.api.http.{ContentTypeOf, ContentTypes, Writeable}
+import play.api.libs.json._
+import play.api.mvc.Codec
+import scalikejdbc.{DB, DBSession, NoExtractor, ReadOnlyAutoSession, SQL, WrappedResultSet}
+
+import scala.language.implicitConversions
+
+/**
+  * Created by jungwh on 2016-10-03.
+  */
+object sqlConverter {
+  def toMap(rs: WrappedResultSet): Map[String, Any] = {
+    (1 to rs.metaData.getColumnCount).foldLeft(Map[String, Any]()) { (result, i) =>
+      val label = rs.metaData.getColumnLabel(i)
+      val a2: Option[Map[String, Any]] = Some(rs.any(i)).map(nullableValue => result + (label -> nullableValue))
+      a2.getOrElse(result)
+    }
+  }
+
+  implicit val AnyMapConverter = new Writes[Map[String, Any]] {
+    def writes(target: Map[String, Any]): JsObject = {
+      target.foldLeft(JsObject(Seq()))((result, pair) =>
+        pair._2 match {
+          case value: String => result ++ Json.obj(pair._1 -> JsString(value))
+          case value: Boolean => result ++ Json.obj(pair._1 -> JsBoolean(value))
+          case value: Int => result ++ Json.obj(pair._1 -> JsNumber(value))
+          case value: Long => result ++ Json.obj(pair._1 -> JsNumber(value))
+          case value: Double => result ++ Json.obj(pair._1 -> JsNumber(value))
+          case value: java.math.BigDecimal => result ++ Json.obj(pair._1 -> JsNumber(value))
+          case value: java.sql.Timestamp => result ++ Json.obj(pair._1 -> Json.toJson(value))
+          case null => result ++ Json.obj(pair._1 -> JsNull)
+        }
+      )
+    }
+  }
+
+  private def sqlToJson(sql: SQL[Nothing, NoExtractor]): JsValue = {
+    val mapList = DB readOnly (implicit session => sql.stripMargin.fetchSize(1000).map(toMap(_)).list().apply())
+    Json.toJson(mapList)
+  }
+
+  def sqlToJsonOutput(sql: SQL[Nothing, NoExtractor]): JsonResult = {
+    val sqlJson = sqlToJson(sql)
+    JsonResult(sqlJson)
+  }
+
+  implicit val resultJsonConverter = new Writes[JsonResult] {
+    def writes(t: JsonResult) = {
+      Json.obj("data" -> t.data, "errorMsg" -> t.errorMsg)
+    }
+  }
+
+  case class JsonResult(data: JsValue = Json.toJson(""), errorMsg: String = "", statusCode: Int = 500)
+
+  object JsonResult {
+    implicit def writeableOf_JsonResult(implicit codec: Codec): Writeable[JsonResult] = {
+      Writeable(jsonResult => codec.encode(Json.toJson(jsonResult).toString))
+    }
+
+    implicit def contentTypeOf_JsonResult(implicit codec: Codec): ContentTypeOf[JsonResult] = {
+      ContentTypeOf[JsonResult](Some(ContentTypes.JSON))
+    }
+  }
+
+  def sqlExist(sql: SQL[Nothing, NoExtractor])(implicit session: DBSession = ReadOnlyAutoSession): Boolean = {
+    val result: Option[String] = sql.stripMargin.map(_.string(1)).first().apply()
+    result match {
+      case Some(t) => true
+      case None => false
+    }
+  }
+}
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,8 @@
 package controllers
 
+import common.sqlConverter._
 import javax.inject._
+
 import play.api.libs.json.Json
 import play.api.mvc._
 import scalikejdbc._
@@ -8,21 +10,87 @@ import scalikejdbc._
 @Singleton
 class HomeController @Inject() extends Controller {
 
-  def index = Action {
-    val userId: List[String] = DB readOnly { implicit session =>
-      sql"select USER_ID from USER_INFO"
-        .map(rs => rs.string("USER_ID"))
-        .list()
-        .apply()
+  object UserInfo {
+    implicit val residentWrites = Json.writes[UserInfo]
+  }
+  case class UserInfo (userId: String, userPassword: String)
+
+  def login(requestUserId : String,
+            requestUserPassword : String) = Action {
+    val isUserValidation: Boolean = sqlExist(sql"""SELECT "USER_ID", "USER_PASSWORD"
+                                FROM "USER_INFO"
+                                WHERE "USER_ID" = $requestUserId
+                                AND "USER_PASSWORD" = $requestUserPassword
+                            """)
+
+    val resultMap: Map[String, Boolean] = {
+      Map[String, Boolean]("result" -> isUserValidation)
     }
 
+    isUserValidation match {
+      case true => Ok(JsonResult(Json.toJson(resultMap)))
+      case false => Ok(JsonResult(Json.toJson(resultMap), errorMsg = "로그인정보가 잘못됬습니다."))
+    }
 
-    // except conf file test
+  }
 
 
-    val a = Json.toJson(userId)
+  implicit val RegisterDataConverter = Json.reads[RegisterData]
 
-    Ok(a)
+  case class RegisterData(USER_ID: String,
+                          USER_PASSWORD: String,
+                          USER_NM: String,
+                          MOBLPHON: String,
+                          EMAIL: String){
+    def validateInsert(): Option[JsonResult] = {
+      /*val programLevel: Int = PROGRAM_GB match {
+        case "메뉴" => 0
+        case "프로그램" => 1
+      }
+      val isProgramIdExists = CodeSearch.programIdExists(PROGRAM_ID, programLevel)
+      val isProgramNameEmpty = PROGRAM_NAME match {
+        case "" => false
+        case _ => true
+      }*/
+
+      //(isProgramIdExists, isProgramNameEmpty) match {
+      (false, false) match {
+        case (true,false) => Some(JsonResult(errorMsg = "이미 존재하는 프로그램ID입니다."))
+        case (true,true) => Some(JsonResult(errorMsg = "이미 존재하는 프로그램ID이며, 프로그램명이 비었습니다."))
+        case (false, false) => Some(JsonResult(errorMsg = "프로그램명이 비었습니다."))
+        case (false, true) => None
+      }
+    }
+  }
+
+  def toSqlParam(target: Seq[RegisterData]): Seq[Seq[(Symbol, Any)]] = {
+    target.map(t => Seq(
+      'USER_ID -> t.USER_ID,
+      'USER_PASSWORD -> t.USER_PASSWORD,
+      'USER_NM -> t.USER_NM,
+      'MOBLPHON -> t.MOBLPHON,
+      'EMAIL -> t.EMAIL
+    ))
+  }
+
+  def register() = Action { implicit req =>
+    val body = req.body.asJson.get
+    val data = body.as[Seq[RegisterData]]
+    val validationResult = data.toStream.flatMap(t => t.validateInsert()).headOption
+    validationResult match {
+      case Some(t) => BadRequest(t)
+      case None =>
+
+        val insertSqlParam = toSqlParam(data)
+        DB localTx { implicit session =>
+          sql"""
+             INSERT INTO "USER_INFO"("USER_ID", "USER_PASSWORD", "USER_NM", "MOBLPHON", "EMAIL", "USE_AT", "REG_DATE", "LOG_TM")
+             VALUES ({USER_ID}, {USER_PASSWORD}, {USER_NM}, {MOBLPHON}, {EMAIL}, '1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+            """
+            .batchByName(insertSqlParam: _*).apply()
+        }
+        Ok
+    }
   }
 
 }
